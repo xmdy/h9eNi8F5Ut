f210988c21e5b8e87d4fa5d265c2e5fb2e41f507
xmdy <xmdy.pro@gmail.com>
2016-12-03 18:44:33 +0300

    add helper scripts

---
+++ scripts/venv_create.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT="$(dirname $SCRIPT_DIR)"
+
+VENV=${ROOT}/venv
+rm -rf ${VENV}
+virtualenv ${VENV}
+source $VENV/bin/activate
+pip install --upgrade pip
+pip install --upgrade setuptools
+pip --no-cache-dir install -r $ROOT/requirements.txt
